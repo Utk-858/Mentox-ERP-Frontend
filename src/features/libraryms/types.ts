17e8f86b7067d787b3a89cec8dc28441690622b7
Utk-858 <151568946+Utk-858@users.noreply.github.com>
2025-06-06 01:56:37 +0530

    vercel bug in library fix

--- src/features/libraryms/types.ts
+++ src/features/libraryms/types.ts
@@ -1,5 +1,14 @@
+// src/features/libraryms/types.ts
+import type React from "react";
+
 export type Category = {
   label: string;
-  icon: React.JSX.IntrinsicElements.img;
+  icon: React.JSX.Element;
 };
 
+export type Book = {
+  id: string;
+  title: string;
+  author: string;
+  cover: string;
+};
